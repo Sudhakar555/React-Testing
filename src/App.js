fac04ba61d005c7e91fd753a0a809fb51b6ef190
stgadmin <stgadmin@syntbotslab.com>
2020-08-23 18:02:19 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
-    </div>
-  );
+ class App extends Component{
+
+  state = {
+    count:0
+  }
+
+  
+
+  handleClick = () => {
+    this.setState({count:this.state.count + 1})
+  }
+  render(){
+    return (
+      <div data-test='component-app'>
+        <h1 data-test='count-display' >No of Times Button Clicked:{this.state.count}</h1>
+        <button data-test='increment-button' onClick={this.handleClick}>Increment Value</button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme,{shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });//new instance
+
+const setup = (props={}, state=null) => {
+
+    return shallow(<App {...props} />)
+
+}
+
+test('renders without crashing', () => {
+ const wrapper =  shallow(<App/>)
+ const appComponent= wrapper.find("[data-test='component-app']")
+ expect(appComponent.length).toBe(1);
 });
+
+test('renders increment button', () => {
+    const wrapper =  shallow(<App/>)
+    const button= wrapper.find("[data-test='increment-button']")
+    expect(button.length).toBe(1);
+})
+
+test('renders count display', () => {
+    const wrapper =  shallow(<App/>)
+    const countDisplay= wrapper.find("[data-test='count-display']")
+    expect(countDisplay.length).toBe(1);
+})
+
+test('conter starts at 0', () => {
+
+})
+
+test ('renders clicking button increments counter display', () => {
+
+})
